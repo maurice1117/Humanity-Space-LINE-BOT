fe083030e9e81130525b793566073fd0fc88fd1e
Maurice <maurice8huang@gmail.com>
2025-04-13 22:54:12 +0800

    撰寫/callback endpoint

--- app.py
+++ app.py
@@ -1,6 +1,31 @@
-from flask import Flask
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+import os
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+line_bot_api = LineBotApi(os.getenv('Z+llILjkg9Vnn4ARo3PFU8rDgsW5QeW61znOSXIvG1NrJ2icoNad867GhgmUzKeLRqybUhnXabEUAzBs11ZgvXldXBe3VvuRITL792wC1ZgcVuvPSiQqBkes5bGPL+Sczrs/WVGx2zcMt6Rf3AkUEwdB04t89/1O/w1cDnyilFU='))
+handler = WebhookHandler(os.getenv('99df85c3a61d7082c15bb4928b531399'))
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+# echo robot
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)
+    )
