65c055e748f3eb23ce653f31401cf70e0975f081
Maurice <maurice8huang@gmail.com>
2025-04-13 23:04:05 +0800

    將環境變數新增至.env檔中

--- app.py
+++ app.py
@@ -2,13 +2,19 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from dotenv import load_dotenv
 
 import os
 
+load_dotenv()
+
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(os.getenv('Z+llILjkg9Vnn4ARo3PFU8rDgsW5QeW61znOSXIvG1NrJ2icoNad867GhgmUzKeLRqybUhnXabEUAzBs11ZgvXldXBe3VvuRITL792wC1ZgcVuvPSiQqBkes5bGPL+Sczrs/WVGx2zcMt6Rf3AkUEwdB04t89/1O/w1cDnyilFU='))
-handler = WebhookHandler(os.getenv('99df85c3a61d7082c15bb4928b531399'))
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
+channel_secret = os.getenv('LINE_CHANNEL_SECRET')
+
+line_bot_api = LineBotApi(channel_access_token)
+handler = WebhookHandler(channel_secret)
 
 @app.route("/callback", methods=['POST'])
 def callback():
